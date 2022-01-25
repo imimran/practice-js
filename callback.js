b969786fb9a91fb3fc10faee55bcc313aac7afc6
imimran <engr.aih@gmail.com>
2022-01-25 23:10:37 +0600

    Object, Callback

---
+++ callback.js
@@ -0,0 +1,49 @@
+// function display(result){
+//     console.log("result is " + result);
+// }
+
+// function add(a, b, callback){
+//     let sum = a + b
+//     if(callback) callback(sum)
+//     return sum
+// }
+
+// const total = add(10, 5, display)
+// console.log(total);
+
+
+const payment = true
+const mark = 70
+
+function enroll(callback){
+    console.log("Enroll progress...");
+    setTimeout( function(){
+
+  
+    if(payment){
+        callback()
+    }else{
+        console.log("Payment fail");
+    }  
+  }, 2000)
+}
+
+function progress(callback){
+    console.log("Progressing...");
+
+    setTimeout( function(){
+    if(mark>60){
+        callback()
+    }else{
+        console.log("Fail");
+    }
+}, 3000)
+}
+
+function getCertificate(){
+    console.log("congrats");
+}
+
+enroll(function(){
+    progress(getCertificate)
+})
---
+++ object.js
@@ -0,0 +1,10 @@
+function Person(firstName, lastName, age, education){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.education = education
+    
+}
+
+const Imran = new Person( "Imran", "Hossain", 29, "BSc")
+console.log(Imran);
